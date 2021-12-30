4e18627d7d58237db0a7b9b12b3f36f86d0aa2d6
tonyykosseifyy <tonykosseify123@gmail.com>
2021-12-30 21:46:43 +0200

    2 files modified

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,7 +1,6 @@
 import Commerce from '@chec/commerce.js';
 
-const checAPIKey = process.env.REACT_APP_CHEC_PUBLIC_KEY;
-const devEnvironment = process.env.NODE_ENV === 'development';
+const checAPIKey = "";
 
 const commerceConfig = {
   axiosConfig: {
@@ -12,13 +11,13 @@ const commerceConfig = {
   },
 };
 
-if (devEnvironment && !checAPIKey) {
+if (!checAPIKey) {
   throw Error('Your public API key must be provided as an environment variable named NEXT_PUBLIC_CHEC_PUBLIC_KEY. Obtain your Chec public key by logging into your Chec account and navigate to Setup > Developer, or can be obtained with the Chec CLI via with the command chec whoami');
 }
 
 export default new Commerce(
   checAPIKey,
-  devEnvironment,
+  true,
   commerceConfig,
 );
 
