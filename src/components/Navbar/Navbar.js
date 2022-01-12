0717ee88d8f456ac6d2bdfeea236c4af5b44df45
tonyykosseifyy <tonykosseify123@gmail.com>
2022-01-12 23:12:04 +0200

    1 file modified

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./Navbar.css";
 
+const links = [
+  "products",
+  "features",
+  "pricing",
+  "",
+  "login"
+];
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo"></div>
       <div className="navbar-links">
-        
+        { links.map(link => (
+          <a key={link} href={link}>{link}</a>
+        ))}
       </div>
     </nav>
   );
