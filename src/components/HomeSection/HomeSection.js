e6c1556702beb961318ff9ebde6d6f3b9cc2c61e
tonyykosseifyy <tonykosseify123@gmail.com>
2022-01-13 19:38:21 +0200

    2 files modified

--- src/components/HomeSection/HomeSection.js
+++ src/components/HomeSection/HomeSection.js
@@ -5,7 +5,13 @@ import "./HomeSection.css";
 const HomeSection = () => {
   return (
     <div className="homesection">
-      this is home section
+      <div className="homesection-left">
+        <div className="homesection-left-heading">
+          <div className="homesection-left-new"><span>NEW</span></div>
+          <h3>monograph dashboard</h3>
+        </div>
+      </div>
+      <div className="homesection-right"></div>
     </div>
   );
 };
