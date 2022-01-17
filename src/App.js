86cbccf36ffa29b6ee2ac4db940e32cb0912c1bd
tonyykosseifyy <tonykosseify123@gmail.com>
2022-01-17 22:26:54 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,26 +2,12 @@ import React from "react";
 import "./index.css";
 import styled from "styled-components";
 import { Navbar , HomeSection } from "./components";
-import { BrowserRouter as Route , Router , Switch } from "react-router-dom"
-
 
 const App = () => {
   return (
     <AppWrapper className="app">
-      <Route>
-        <Switch>
-         
-          <Router exact path="/">
-            <Navbar />
-            <HomeSection />
-          </Router>
-          
-          <Router path="/features">
-            featuress
-          </Router>
-        </Switch>
-        
-      </Route>
+      <Navbar />
+      <HomeSection />
     </AppWrapper>
   );
 }
